c9a8177f3ff5fd085c9173c8c404649df35ff8e4
Abie <abiefaz@gmail.com>
2021-11-18 18:17:17 -0600

    transition between xml files in navigation bar

--- app/src/main/java/com/example/grapevine/MainActivity.java
+++ app/src/main/java/com/example/grapevine/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.grapevine;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.homepage:
+                        // do something here
+                        return true;
+                    case R.id.search:
+                        setContentView(R.layout.activity_search);
+                        return true;
+                    case R.id.post:
+                        setContentView(R.layout.activity_post);
+                        return true;
+                    case R.id.profile:
+                        setContentView(R.layout.activity_profile);
+                        return true;
+                    default: return true;
+                }
+            }
+        });
     }
 }
