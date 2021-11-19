08310d7e216a1cbfb8908d766dc167438114a71c
Abie <abiefaz@gmail.com>
2021-11-18 20:54:19 -0600

    Added fragments and modified navigation bar
    Added Feed, Post, Profile, and Search Fragments for the navigation bar.

--- app/src/main/java/com/example/grapevine/MainActivity.java
+++ app/src/main/java/com/example/grapevine/MainActivity.java
@@ -5,36 +5,51 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.grapevine.fragments.FeedFragment;
+import com.example.grapevine.fragments.PostFragment;
+import com.example.grapevine.fragments.ProfileFragment;
+import com.example.grapevine.fragments.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setItemIconTintList(null);
 
         bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = new FeedFragment();
                 switch (item.getItemId()) {
                     case R.id.homepage:
-                        // do something here
-                        return true;
+                        fragment = new FeedFragment();
+                        break;
                     case R.id.search:
-                        setContentView(R.layout.activity_search);
-                        return true;
+                        fragment = new SearchFragment();
+                        break;
                     case R.id.post:
-                        setContentView(R.layout.activity_post);
-                        return true;
+                        fragment = new PostFragment();
+                        break;
                     case R.id.profile:
-                        setContentView(R.layout.activity_profile);
-                        return true;
-                    default: return true;
+                        fragment = new ProfileFragment();
+                        break;
+                    default:
+                        break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.rlContainer, fragment).commit();
+                return true;
             }
         });
+        bottomNavigationView.setSelectedItemId(R.id.homepage);
     }
 }
---
+++ app/src/main/java/com/example/grapevine/fragments/FeedFragment.java
@@ -0,0 +1,36 @@
+package com.example.grapevine.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.grapevine.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FeedFragment extends Fragment {
+
+    public FeedFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.feedfragment, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
