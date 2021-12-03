0ecc7e004c3678dbb898769f7618b0e0fdd5da67
Ethan Chen <chengox12@gmail.com>
2021-12-03 12:41:12 -0600

    Connected to Database
    Connected to our database

--- app/src/main/java/com/example/grapevine/ParseApplication.java
+++ app/src/main/java/com/example/grapevine/ParseApplication.java
@@ -14,8 +14,8 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("B5OrZ6lgxOaVI466eZeIsuCh9vpOpYSqJ0ffd8ef")
-                .clientKey("PZugnwRUrYrnALqPKWeUZgdB6PTJ3aC8W9AcGdpd")
+                .applicationId("9vmH03mv4S8jkyYFtfs3Q1XHjsFGIVM3SaZBAzdE")
+                .clientKey("pWjpv1VCcKsjuNBgzAeYc92GZvxXmBmUJEfGnePI")
                 .server("https://parseapi.back4app.com")
                 .build()
         );
